f0ac17016388300d1256b04dd98c86c8348a66ca
William Santiago Alzate <santiago_alza@hotmail.com>
2019-12-13 16:30:25 -0500

    Editada seccion de filter

--- src/components/filter.js
+++ src/components/filter.js
@@ -6,9 +6,48 @@ import '../css/filter.css';
 
 //Create component
 export default class Filter extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          minInput: "",
+          maxInput: "",
+          minOutput: 0,
+          maxOutput: 0
+        };
+    }
+
+    format = e => {
+        var num = e.target.value.replace(/\,/g,'');
+
+        num = num.toString().split('').reverse().join('').replace(/(?=\d*\,?)(\d{3})/g,'$1,');
+        num = num.split('').reverse().join('').replace(/^[\,]/,'');
+
+        if (e.target.name==="min") {
+            this.setState({
+                minInput: e.target.value,
+                minOutput: num
+            })
+        } else {
+            this.setState({
+                maxInput: e.target.value,
+                maxOutput: num
+            })
+        }
+
+    };
+
     render() {
         return (
             <div className="filter">
+                <div className="container m-auto" style={this.props.style}>
+                    <a href="#" className="filters col-md-12">
+                        <h1>Filter 1</h1>
+                    </a>
+                    <a href="#" className="filters col-md-12">
+                        <h1>Filter 2</h1>
+                    </a>
+                </div>
                 <div className="modal fade" id="price-modal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div className="modal-dialog" role="document">
                         <div className="modal-content">
@@ -19,11 +58,20 @@ export default class Filter extends Component {
                                 </button>
                             </div>
                             <div className="modal-body">
-                                <form>
-                                    <div class="form-group">
-                                        <label for="formControlRange">Example Range input</label>
-                                        <input type="range" class="form-control-range" id="formControlRange"/>
+                                <form className="row">
+                                    <div className="input-group col-md-6">
+                                        <div className="input-group-prepend">
+                                            <div className="input-group-text">$</div>
+                                        </div>
+                                        <input type="number" className="form-control" autoComplete="off" placeholder="Min" name="min" value={this.state.minInput} onChange={this.format}/>
                                     </div>
+                                    <div className="input-group col-md-6">
+                                        <div className="input-group-prepend">
+                                            <div className="input-group-text">$</div>
+                                        </div>
+                                        <input type="number" className="form-control" autoComplete="off" placeholder="Max" name="max" value={this.state.maxInput} onChange={this.format}/>
+                                    </div>
+                                    <label className="col-md-12 mt-3 text-center">${this.state.minOutput} - ${this.state.maxOutput}</label>
                                 </form>
                             </div>
                             <div className="modal-footer">
--- src/components/user.js
+++ src/components/user.js
@@ -17,31 +17,31 @@ export default class User extends Component {
                         <form action="">
                             <div className="input-group mb-3">
                                 <div className="input-group-prepend">
-                                    <i className="fas fa-user input-group-text"></i>
+                                    <i className="fas fa-user input-group-text user-icons"></i>
                                 </div>
                                 <input type="text" className="form-control" placeholder="Name"/>
                             </div>
                             <div className="input-group mb-3">
                                 <div className="input-group-prepend">
-                                    <i className="fas fa-calendar input-group-text"></i>
+                                    <i className="fas fa-calendar input-group-text user-icons"></i>
                                 </div>
                                 <input type="number" className="form-control" placeholder="Age"/>
                             </div>
                             <div className="input-group mb-3">
                                 <div className="input-group-prepend">
-                                    <i className="fas fa-phone input-group-text"></i>
+                                    <i className="fas fa-phone input-group-text user-icons"></i>
                                 </div>
                                 <input type="number" className="form-control" placeholder="Phone number"/>
                             </div>
                             <div className="input-group mb-3">
                                 <div className="input-group-prepend">
-                                    <i className="fas fa-envelope input-group-text"></i>
+                                    <i className="fas fa-envelope input-group-text user-icons"></i>
                                 </div>
                                 <input type="email" className="form-control" placeholder="Email"/>
                             </div>
                             <div className="input-group mb-3">
                                 <div className="input-group-prepend">
-                                    <i className="fas fa-key input-group-text"></i>
+                                    <i className="fas fa-key input-group-text user-icons"></i>
                                 </div>
                                 <input type="password" className="form-control" placeholder="Password"/>
                             </div>
